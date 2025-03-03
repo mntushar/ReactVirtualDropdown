4186855f6ecbfdb71d05a83ad7e8b78985adb110
M N Tushar <M N Tushar@DESKTOP-QEJDQPG>
2025-03-03 16:29:02 +0600

    testing npm package

--- testing/testing-code/app/page.tsx
+++ testing/testing-code/app/page.tsx
@@ -1,9 +1,11 @@
+import { sayHello } from "react-virtual-dropdown";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Alhadmulilah</h1>
+      <h2>{sayHello()}</h2>
     </div>
   );
 }
