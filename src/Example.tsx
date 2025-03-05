1474c7cdf5c1fc3a9cafb8f81c95d9f08321853b
M N Tushar <M N Tushar@DESKTOP-QEJDQPG>
2025-03-05 17:26:34 +0600

    add example

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,66 +1,74 @@
 'use client'
 
-import { ToastHandler } from "@/app/_shared/component/toast";
-import { SelectItem, SelectorRequest, VirtualSelector } from "@/app/_shared/component/virtual_selector";
-import Category from "@/request_handlers/category";
-import { useRouter } from "next/navigation";
-import { Fragment, useCallback, useMemo } from "react";
+import React from "react";
+import { useCallback, useEffect, useState } from "react";
+import { SelectItem, SelectorRequest, VirtualSelector } from "react-virtual-dropdown";
 
 export default function Dashboard() {
-  const route = useRouter();
-  const selectedData = '';
-  const requestHandler = useMemo(() => new Category(route), [route]);
-
+  const [selectedData, setSelectedData] = useState<string>('');
   const fetchData = useCallback(async (request: SelectorRequest) => {
     try {
       const params = new URLSearchParams({
-            skip: request.startIndex.toString(),
-            limit: request.limit.toString(),
-            sortColumn: 'name,
-            sortOrder: '',
-            searchKey: searchKey,
-        });
-      const url = `${http://localhost:3000}/list?${params}`;
+        skip: request.startIndex.toString(),
+        limit: request.limit.toString(),
+        sortColumn: 'name',
+        sortOrder: '',
+        searchKey: request.searchKey ?? '',
+      });
+      const url = `https://your_url/comments?${params}`;
       const response = await fetch(url);
-      if(!response.ok) throw new Error();
+      if (!response.ok) throw new Error();
       const data = await response.json();
-      const itemData = data.map(({ id, name }: SelectItem) => ({ id, name }))
-  
-      const url = `${http://localhost:3000}/count?${searchKey}`;
-      const response = await fetch(url);
-      if(!response.ok) throw new Error();
-      const count = await response.json();
+      const itemData = data.map(({ id, email }: { id: number, email: number }) => ({
+        id: id.toString(),
+        name: email.toString()
+      }));
+
+      const countUrl = `http://your_url/count?searchKey=${request.searchKey}`;
+      const countResponse = await fetch(countUrl);
+      if (!countResponse.ok) throw new Error();
+      const count = await countResponse.json();
       return {
         items: itemData,
         totalCount: count,
       };
     } catch (error) {
-      ToastHandler.Error((error as Error).message);
       console.error("Error fetching data:", error);
       return {
         items: [],
         totalCount: 0,
       };
     }
-  }, [requestHandler]);
+  }, []);
 
   const getValue = (data: SelectItem) => {
     console.log(data.id, data.name);
   };
 
-  return (
-    <Fragment>
-      <div className="m-3">
-        <h1>Dashboard</h1>
+  const getSetData = async () => {
+    const countUrl = `http://your_url/your-data-id`;
+    const countResponse = await fetch(countUrl);
+    if (!countResponse.ok) throw new Error();
+    const data = await countResponse.json();
+    setSelectedData(data);
+  };
 
+  useEffect(() => {
+    getSetData()
+  }, [])
+
+  return (
+    <div>
+      <h1>Alhadmulilah</h1>
+      <div style={{ width: "500px" }}>
         <VirtualSelector
           fetchData={fetchData}
           height={200}
           rowHeight={35}
-          placeholder="Select Category"
+          placeholder="Select Dropdown"
           selectedData={selectedData}
           callBack={getValue} />
       </div>
-    </Fragment>
+    </div>
   );
 }
